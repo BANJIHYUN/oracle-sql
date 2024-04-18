e0c1a04400f08e1789ab9d2e79e4f47bb51619fb
ghban <ghban@DESKTOP-5PS6J6K>
2024-04-18 10:17:16 +0900

    [반지현]dept emp 테이블 생성

---
+++ src/main/java/vo/Dept.java
@@ -0,0 +1,10 @@
+package vo;
+
+// VO(Value Object)> DTO(Data Transfer Object) Domain(속성 값의 범위)
+
+public class Dept {
+
+	public int deptNo;
+	public String dname;
+	public String loc;
+}
---
+++ src/main/java/vo/Emp.java
@@ -0,0 +1,14 @@
+package vo;
+
+public class Emp {
+	int empno;
+	String ename;
+	String job;
+	int MGR;
+	String hiredate;
+	int sal; 
+	int comm;
+	int deptno;
+	
+	
+}
